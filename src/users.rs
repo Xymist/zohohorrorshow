7a78977549070c1867bd9dd61713a4cfd06fc9e4
Xymist <james.duerden@marketdojo.com>
2018-03-03 15:09:53 +0000

    Add Travis config

--- src/users.rs
+++ src/users.rs
@@ -1,3 +1,9 @@
+use errors::*;
+use RelativePath;
+
+type ProjectUsers = ZohoUsers;
+type PortalUsers = ZohoUsers;
+
 #[derive(Debug, Serialize, Deserialize)]
 pub struct ZohoUsers {
     #[serde(rename = "users")]
@@ -14,4 +20,22 @@ pub struct User {
     pub email: String,
     #[serde(rename = "role")]
     pub role: String,
+}
+
+impl<'a> RelativePath<[i64; 2]> for ProjectUsers {
+    fn relative_path(params: [i64; 2]) -> Result<String> {
+        Ok(format!(
+            "portal/{}/projects/{}/users/",
+            params[0], params[1]
+        ))
+    }
+}
+
+impl<'a> RelativePath<i64> for PortalUsers {
+    fn relative_path(params: i64) -> Result<String> {
+        Ok(format!(
+            "portal/{}/users/",
+            params[0]
+        ))
+    }
 }
