f7a2d00d4d0206e919dd37a84c234ed5c66a2ac6
Xymist <james@jamieduerden.me>
2018-04-03 23:06:28 +0100

    Implement name and ID filter for milestones

--- src/models/milestone.rs
+++ src/models/milestone.rs
@@ -24,10 +24,84 @@ pub struct MilestoneFragment {
 impl MilestoneFragment {
     query_strings!(MilestoneFragment; index, range, status, display_type, flag);
 
+
+    // Fetch a specific portal by id
+    pub fn by_id(self, id: i64) -> MilestoneFilter {
+        let mut path_frags = self.path.split('?').collect::<Vec<&str>>();
+        if path_frags[1].contains('&') {
+            let autht = path_frags.remove(1).split('&').collect::<Vec<&str>>()[0];
+            path_frags.push(autht)
+        }
+        MilestoneFilter {
+            client: Rc::clone(&self.client),
+            path: format!("{}{}/?{}", path_frags[0], id, path_frags[1]),
+            filter: Filter::ID(id),
+        }
+    }
+
+    // Fetch a specific portal by name
+    pub fn by_name(self, name: &str) -> MilestoneFilter {
+        if self.path.contains('&') {
+            panic!("Cannot both filter and find by name")
+        }
+        MilestoneFilter {
+            client: Rc::clone(&self.client),
+            path: self.path,
+            filter: Filter::Name(name.to_owned()),
+        }
+    }
+
     // Execute the query against the Zoho API
     pub fn fetch(self) -> Result<Vec<Milestone>> {
         let milestone_list: ZohoMilestones = self.client.get(&self.path)?;
         Ok(milestone_list.milestones)
+    }
+}
+
+#[derive(Debug)]
+enum Filter {
+    ID(i64),
+    Name(String),
+}
+
+#[derive(Debug)]
+pub struct MilestoneFilter {
+    client: Rc<ZohoClient>,
+    path: String,
+    filter: Filter,
+}
+
+impl MilestoneFilter {
+    // Execute the query against the Zoho API
+    pub fn fetch(self) -> Result<Option<Milestone>> {
+        let milestone_list: ZohoMilestones = self.client.get(&self.path)?;
+        let milestones = milestone_list.milestones;
+        match self.filter {
+            Filter::ID(id) => filter_by_id(milestones, id),
+            Filter::Name(name) => filter_by_name(milestones, &name),
+        }
+    }
+}
+
+fn filter_by_id(milestones: Vec<Milestone>, id: i64) -> Result<Option<Milestone>> {
+    let mut filtered = milestones
+        .into_iter()
+        .filter(|m| m.id == id)
+        .collect::<Vec<Milestone>>();
+    match filtered.len() {
+        0 => Ok(None),
+        _ => Ok(Some(filtered.remove(0))),
+    }
+}
+
+fn filter_by_name(milestones: Vec<Milestone>, name: &str) -> Result<Option<Milestone>> {
+    let mut filtered = milestones
+        .into_iter()
+        .filter(|m| m.name == name)
+        .collect::<Vec<Milestone>>();
+    match filtered.len() {
+        0 => Ok(None),
+        _ => Ok(Some(filtered.remove(0))),
     }
 }
 
