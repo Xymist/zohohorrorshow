34b453033b0a26fbd8a9ed491787a6d7bfb8db5e
Xymist <james.duerden@marketdojo.com>
2019-01-04 17:17:15 +0000

    2018 Edition Imports

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,14 @@
-extern crate chrono;
+//! # Zohohorrorshow
+//!
+//! A library for interacting with the Zoho Projects API, because it's an awful nightmare of ambiguous fields,
+//! optional fields, random casing and largely absent documentation. Xорошо́!
+
 #[macro_use]
 extern crate error_chain;
 #[macro_use]
 extern crate lazy_static;
-extern crate mockito;
-extern crate reqwest;
-extern crate serde;
 #[macro_use]
 extern crate serde_derive;
-extern crate serde_json;
 
 #[macro_use]
 mod macros;
